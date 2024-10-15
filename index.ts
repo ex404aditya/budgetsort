9c1f66657554a8e74fbdfc7abf1706620009594a
ex404aditya <ex404ug@gmail.com>
2024-10-16 02:17:39 +0530

    bun serve

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!sum bithc");
+
+
+const port = process.env.PORT || 4000
+
+Bun.serve({
+    port: port,
+    fetch(req) {
+        return new Response("first time from bun");
+    }
+})
+
+console.log("server running!sum bithc in the cosmos");
