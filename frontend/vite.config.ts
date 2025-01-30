fb81d471b502acb71b36a157804b10e88885fab3
ex404aditya <ex404ug@gmail.com>
2025-01-30 23:06:49 +0530

    tailwind init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dir, "./src"),
+      "@server": path.resolve(import.meta.dir, "../server"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,13 @@
-import app from './app'
+import app from "./app";
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 Bun.serve({
-    port: port,
-    static: {
-        "/": new Response("sum bithc in the cosmos")
-    },
-    fetch: app.fetch
-})
+  port: port,
+  static: {
+    "/": new Response("sum bithc in the cosmos"),
+  },
+  fetch: app.fetch,
+});
 
 console.log("server running!");
